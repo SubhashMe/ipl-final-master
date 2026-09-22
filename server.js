06ffb0d460a5ce0223aad7faf635704748201a3e
Subhash Meravi <subhashmeravi@gmail.com>
2026-09-23 00:08:44 +0530

    Fix Vercel static routing

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var object=require('./eco.json');
 var express= require('express');
+var path = require('path');
 var app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/eco',(req,res) =>
 {
